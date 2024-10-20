3deb9a880521367b067a52e96aad5cb72e15412f
itsayush30 <itsayush30@gmail.com>
2024-10-20 17:52:39 +0530

    React Memo

---
+++ ReactMemo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+function ChildComponent({ name }) {
+  console.log('ChildComponent rendered');
+  return <div>Hello, {name}!</div>;
+}
+
+export default React.memo(ChildComponent);
